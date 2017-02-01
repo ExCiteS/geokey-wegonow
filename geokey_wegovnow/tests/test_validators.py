688cbefbe127c698e41978946cebbcf063aa6436
Julius <j.osokinas@mappingforchange.org.uk>
2017-02-01 12:36:40 +0000

    Initial test for validators

---
+++ geokey_wegovnow/tests/test_validators.py
@@ -0,0 +1,34 @@
+"""Test all validators."""
+
+from oauthlib.common import Request
+
+from django.test import TestCase
+
+from geokey_wegovnow import validators
+
+
+class UWUMOAuth2ValidatorTests(TestCase):
+    """Tests for UWUM OAuth2 validator."""
+
+    validator = UWUMOAuth2Validator()
+
+    def setUp(self):
+        """Set up tests."""
+        self.token = 'f34mfSADsf45sada31mF'
+        self.scopes = []
+        self.headers = {
+            'Authorization': 'Bearer %s' % access_token,
+            'UWUM': True,
+        }
+        self.request = Request(
+            'https://geokey.org.uk/api/projects/',
+            http_method='GET',
+            body=None,
+            headers=self.headers)
+
+    def test_validate_bearer_token_when_no_token(self):
+        """Test checking bearer token when token is not provided."""
+        self.assertFalse(validator.validate_bearer_token(
+            None,
+            self.scopes,
+            self.request))
